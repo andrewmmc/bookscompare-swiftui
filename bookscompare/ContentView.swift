a1ad44bffcf1bc10ab0fdefd161f5851f80d3227
Andrew Mok <andrewmmc@users.noreply.github.com>
2019-06-07 09:31:22 -0700

    Initial Commit

---
+++ bookscompare/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  bookscompare
+//
+//  Created by Andrew Mok on 7/6/2019.
+//  Copyright © 2019 Andrew Mok. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
